dfae68c753801ef2422bfeab86067e5a8d80a6c5
Ratnesh Pasi <ratneshpasi03@gmail.com>
2025-03-13 13:03:12 +0530

    Refactor HomePage component to use hooks for fetching folders and manage loading state

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,31 @@
-export default async function HomePage() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/folders`);
+"use client"
 
-  const folders = await res.json();
+import { useEffect, useState } from "react";
+
+export default function HomePage() {
+
+  const [folders, setFolders] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    async function fetchFolders() {
+      setLoading(true)
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/folders`);
+      const temp = await res.json();
+      setFolders(temp)
+      setLoading(false)
+    }
+
+    fetchFolders()
+  }, [])
+
+  if(loading) {
+    return (
+      <div className="min-h-screen w-full content-center text-center">
+        Loading ...
+      </div>
+    )
+  }
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-8">
